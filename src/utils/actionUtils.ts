f5f38061f106b561f622e30e79ab279441967c54
gussan <83329336+toyamagu-2021@users.noreply.github.com>
2024-07-01 01:52:40 +0900

    deps: node 20 (#1)
    Signed-off-by: toyamagu2021 <toyamagu2021@gmail.com>

--- src/utils/actionUtils.ts
+++ src/utils/actionUtils.ts
@@ -99,7 +99,7 @@ export function getInputS3ClientConfig(): S3ClientConfig | undefined {
                 process.env["AWS_SESSION_TOKEN"]
         },
         region: core.getInput(Inputs.AWSRegion) || process.env["AWS_REGION"],
-        endpoint: core.getInput(Inputs.AWSEndpoint),
+        endpoint: core.getInput(Inputs.AWSEndpoint) || undefined,
         bucketEndpoint: core.getBooleanInput(Inputs.AWSS3BucketEndpoint),
         forcePathStyle: core.getBooleanInput(Inputs.AWSS3ForcePathStyle)
     } as S3ClientConfig;
